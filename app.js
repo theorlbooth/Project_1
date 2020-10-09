9ac3855c1a7f510a1b82a0e4e943c35a2be26f26
Theo Booth <theorlbooth@googlemail.com>
2020-10-09 11:39:50 +0100

    Arrays with beg/end indices

--- app.js
+++ app.js
@@ -20,6 +20,19 @@
 //   }
 // }
 
+// function findRowStarts(width) {
+//   for (let i = 1; i <= width; i++) {
+//     gridStarts.push(width * (i - 1))
+//   }
+//   return gridStarts
+// }
+
+// function findRowEnds(width) {
+//   for (let i = 1; i <= width; i++) {
+//     gridEnds.push(((width * (width - (width - i))) - 1))
+//   }
+//   return gridEnds
+// }
 
 // * -------------------------------------------
 // * ------------------ Grid -------------------
@@ -37,6 +50,26 @@ for (let i = 0; i < width ** 2; i++) {
   grid.appendChild(div)
   cells.push(div)
 }
+
+const gridStarts = [0] // * Starting at 0 in order to be able to call the right number based on the row number
+const gridEnds = [0] // * Starting at 0 in order to be able to call the right number based on the row number
+
+function findRowStarts(width) {
+  for (let i = 1; i <= width; i++) {
+    gridStarts.push(width * (i - 1))
+  }
+  return gridStarts
+}
+
+function findRowEnds(width) {
+  for (let i = 1; i <= width; i++) {
+    gridEnds.push(((width * (width - (width - i))) - 1))
+  }
+  return gridEnds
+}
+
+findRowStarts(width)
+findRowEnds(width)
 
 
 // * -------------------------------------------
@@ -70,11 +103,21 @@ document.addEventListener('keydown', (event) => {
 
 
 
+// ! -------------------------------------------
+// ! ----------- Below needs work! -------------
+// ! -------------------------------------------
+
+
+ 
+
+
+
 // * -------------------------------------------
 // * ----------------- bDroids -----------------
 // * -------------------------------------------
 
-let bDroids = [0, 1, 2, 3, 4]
+let bDroids = [0, 1, 2, 3, 4, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
+
 
 function addDroid() {
   const maxInd = bDroids.indexOf(findMax(bDroids))
@@ -97,8 +140,8 @@ function findIndexOfMaxDroid() {
 }
 //  ! This all needs a rethink!!
 
-console.log(findIndexOfMaxDroid(bDroids))
-console.log(findIndexOfMinDroid(bDroids))
+// console.log(findIndexOfMaxDroid(bDroids))
+// console.log(findIndexOfMinDroid(bDroids))
 
 function moveDroidRight() {
   for (let i = 0; i < bDroids.length; i++) {
@@ -147,22 +190,21 @@ function findMax(array) {
 
 let movement = 0
 
-const interval = setInterval(() => {
+// const interval = setInterval(() => {
   // const max = findMax(bDroids)
   // const min = findMin(bDroids)
 
   // ! Add class 'min' and 'max' classes to to min and max droids so that when .droid is removed from them they're still tracked
 
-  if (max < 8 ) {
-    removeDroids()
-    moveDroidRight()
-    addDroid()
-  } else if (max === 8) {
-    removeDroids()
-    moveDroidDown()
-    addDroid()
-  }
+//   if (max < 8 ) {
+//     removeDroids()
+//     moveDroidRight()
+//     addDroid()
+//   } else if (max === 8) {
+//     removeDroids()
+//     moveDroidDown()
+//     addDroid()
+//   }
 
 
-}, 1500);
-
+// }, 1500);
